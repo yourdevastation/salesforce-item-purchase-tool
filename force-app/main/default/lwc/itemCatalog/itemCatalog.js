b1b8e4d371084b19bc51f1b9b81a6a3eb0ff5239
rose-petal <roze_petal@proton.me>
2026-07-11 13:44:37 +0400

    feat: implement centralized error logging and enhance exception handling; refactor security checks and add CDC support for item updates

--- force-app/main/default/lwc/itemCatalog/itemCatalog.js
+++ force-app/main/default/lwc/itemCatalog/itemCatalog.js
@@ -1,5 +1,6 @@
 import { LightningElement, wire, api, track } from "lwc";
 import { refreshApex } from "@salesforce/apex";
+import { subscribe, unsubscribe, onError } from "lightning/empApi";
 import getItems from "@salesforce/apex/ItemPurchaseController.getItems";
 
 export default class ItemCatalog extends LightningElement {
@@ -13,6 +14,42 @@ export default class ItemCatalog extends LightningElement {
   @track allItems = [];
   hasMore = false;
   itemsWireResult;
+
+  channelName = "/data/Item__ChangeEvent";
+  subscription = {};
+
+  connectedCallback() {
+    this.handleSubscribe();
+  }
+
+  disconnectedCallback() {
+    this.handleUnsubscribe();
+  }
+
+  handleSubscribe() {
+    const messageCallback = (response) => {
+      console.log("New CDC Event received: ", JSON.stringify(response));
+      this.refreshList();
+    };
+
+    subscribe(this.channelName, -1, messageCallback).then((response) => {
+      console.log(
+        "Subscription request sent to: ",
+        JSON.stringify(response.channel)
+      );
+      this.subscription = response;
+    });
+
+    onError((error) => {
+      console.error("EMP API Error: ", JSON.stringify(error));
+    });
+  }
+
+  handleUnsubscribe() {
+    unsubscribe(this.subscription, () => {
+      console.log("Unsubscribed from CDC channel");
+    });
+  }
 
   get queryLimit() {
     return this.limitSize + 1;
